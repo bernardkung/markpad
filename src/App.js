7d936e05ca6db53d395ecb5afb38b74ab16d392a
bernardkung <bpkung@gmail.com>
2021-10-04 02:27:31 -0400

    Initialize react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={`inputBox box`}>
+        <input className="inputActual" type="text" />
+        
+
+      </div>
+
+      <div className={`outputBox box`}>output</div>
+    </div>
+  );
+}
+
+export default App;
